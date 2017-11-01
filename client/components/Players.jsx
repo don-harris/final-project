3fc4b6f407fe64733c2d2ac7444c0bb0794c106d
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-11-02 11:08:44 +1300

    icons showing too

--- client/components/Players.jsx
+++ client/components/Players.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
 class Players extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       // players: [{icon: 'icon1', name: 'Bob'}, {icon: null, name: 'Harrison'}]
@@ -12,15 +12,14 @@ class Players extends React.Component {
     }
     this.addPlayer = this.addPlayer.bind(this)
     this.handleChange = this.handleChange.bind(this)
-    this.submitAllPlayers = this.submitAllPlayers.bind(this)
-
+    // this.submitAllPlayers = this.submitAllPlayers.bind(this)
   }
-  handleChange(evt) {
+  handleChange (evt) {
     const {pendingPlayer} = this.state
     pendingPlayer[evt.target.name] = evt.target.value
     this.setState({pendingPlayer})
   }
-  addPlayer(evt) {
+  addPlayer (evt) {
     evt.preventDefault()
     let {players, pendingPlayer} = this.state
     players.push(pendingPlayer)
@@ -28,27 +27,28 @@ class Players extends React.Component {
     this.setState({players, pendingPlayer})
   }
 
-  submitAllPlayers() {
+  // submitAllPlayers () {
 
-  }
+  // }
 
   render () {
     const {players, pendingPlayer} = this.state
     return (
       <form className="form container">
         <div>
-          {players.map(player => <p>{player.name}</p>)}
+          {players.map(player => <p>{player.name} {player.icon}</p>)}
         </div>
         <input className="input" type="text" name="name" placeholder="Add Player Name..." value={pendingPlayer.name} onChange={this.handleChange} />
         <div className="control">
           <div className="select">
-            <select name="icon">
+            <select name="icon" onChange={this.handleChange} >
               <option>Select your icon</option>
               <option>icon 1</option>
               <option>icon 2</option>
               <option>icon 3</option>
               <option>icon 4</option>
               <option>icon 5</option>
+              <option>icon 6</option>
             </select>
           </div>
           <button className="button" onClick={this.addPlayer}>Add Player</button>
