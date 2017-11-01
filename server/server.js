cd730bb35e32c133e67ca56c7830b77a25746979
AnnahGerletti <agerletti@gmail.com>
2017-11-02 10:44:49 +1300

    working routes tested in postman

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const routes = require('/routes/videos')
+const routes = require('./routes/videos')
 
 const server = express()
 
@@ -12,7 +12,7 @@ server.use(cors('*'))
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('api/v1', routes)
+server.use('/api/v1', routes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
