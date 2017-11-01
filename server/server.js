4a4391b3b13b6d8abca6abab6fc2eb9b295e4140
AnnahGerletti <agerletti@gmail.com>
2017-11-02 10:29:00 +1300

    made a videos db and videos routes

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const routes = require('/routes/videos')
+
 const server = express()
 
 server.use(cors('*'))
 
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
+
+server.use('api/v1', routes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
