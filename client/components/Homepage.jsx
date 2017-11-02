0ebf1baaab6fcc243631e7b299b2e2f7dbbc2795
dazyang <dazyang.contact@gmail.com>
2017-11-02 16:47:37 +1300

    need to add image

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
 
 class Homepage extends React.Component {
   constructor (props) {
@@ -11,16 +10,20 @@ class Homepage extends React.Component {
 
   render () {
     return <div className="container">
-      <h1>Home Page</h1>
-      <Link to="/players">
-        <button className="button is large">Get started</button>
-      </Link>
-      <Link to="/contactus">
-        <button className="button is large">Contact Us</button>
-      </Link>
-      <Link to="/instructions">
-        <button className="button is large">Instructions</button>
-      </Link>
+      <div className="main-title">
+        <h1 className="title is-1">Spoken,<br/> Not Stirred</h1>
+        <Link to="/players">
+          <button className="button is large">Get started</button>
+        </Link>
+      </div>
+      <div className="sub-buttons is-level">
+        <Link to="/aboutus" className="level-right">
+          <button className="button is large">About Us</button>
+        </Link>
+        <Link to="/instructions" className="level-right">
+          <button className="button is large">Instructions</button>
+        </Link>
+      </div>
     </div>
   }
 }
