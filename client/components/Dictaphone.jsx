c708bcce2f54c0f90f0aabbe6c3b7ce4608ae358
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2017-11-04 12:15:30 +1300

    edited slightly

--- client/components/Dictaphone.jsx
+++ client/components/Dictaphone.jsx
@@ -12,17 +12,18 @@ const propTypes = {
   browserSupportsSpeechRecognition: PropTypes.bool
 }
 
-var actual = 'Great job, keep it up'
+// var actual = 'Get to the chopper'
+// var actual = this.props.videos.quote
 
-function straightLace (stopListening, transcript) {
-  stopListening()
-  console.log(transcript, actual)
-  if (transcript.toLowerCase() === actual.toLowerCase()) {
-    console.log('Correct!')
-  } else {
-    console.log("That's not it.")
-  }
-}
+// function straightLace (stopListening, transcript) {
+//   stopListening()
+//   console.log(transcript, actual)
+//   if (transcript.toLowerCase() === actual.toLowerCase()) {
+//     console.log('Correct!')
+//   } else {
+//     console.log("That's not it.")
+//   }
+// }
 
 class Dictaphone extends Component {
   constructor (props) {
@@ -39,51 +40,54 @@ class Dictaphone extends Component {
   }
   render () {
     const { transcript, startListening, stopListening, browserSupportsSpeechRecognition } = this.props
-    console.log(this.props)
+    console.log("props =" + this.props)
     function compareText () {
       stopListening()
       var points = 0
+      var actual = 'Get to the chopper'
+      // console.log("randomVid.quote = " + this.state.randomVid.quote)
+      // console.log("this.props.videos = " + this.props.quote)
+      console.log("transcript = " + transcript) // look at final transcript
       transcript.toLowerCase().split(' ').forEach((char, idx, transcriptArr) => {
         const actualArr = actual.toLowerCase().split(' ')
         console.log(actualArr, transcriptArr)
         if (actualArr.find(actualChar => actualChar == char)) points++
       })
       if (transcript.toLowerCase() === actual.toLowerCase()) {
-        console.log('Correct!')
+        console.log('Correct, double points!')
         points = points * 2
       } else {
-        console.log("That's not it.")
+        console.log("Not quite...")
       }
-      console.log(points)
+      console.log("points: " + points)
     }
     if (!browserSupportsSpeechRecognition) {
       return null
     }
     const {randomVid} = this.state
-    console.log(randomVid)
+    console.log("randomVid = " + randomVid)
     return (
       <div>
         <button onClick={startListening}>Speak</button>
-        <button onClick={straightLace.bind(null, stopListening, transcript)}>Stop/Submit</button>
+        <button onClick={compareText.bind(null, stopListening, transcript)}>Stop/Submit</button>
         <br/>
         <input type="text" value={transcript} id="speech-field"/>
         <br/>
         {randomVid && <p>{randomVid.quote}</p>}
-        {randomVid && <Video videoId={randomVid.vid_url} />}
       </div>
     )
   }
 }
 
 Dictaphone.propTypes = propTypes
-
+  
 const options = {
   autoStart: false
 }
 
-const mapStateToProps = ({videos}) => {
+const mapStateToProps = (state) => {
   return {
-    videos
+    videos: state.videos
   }
 }
 
