d46ff175c3726af8fa56b430457bcff38c0e4894
richardjohns <richard.johns193@gmail.com>
2017-11-03 16:28:58 +1300

    5 tests working total yaya

--- test/client/leaderboardTitle.test.js
+++ test/client/leaderboardTitle.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
-import { Provider } from 'react-redux'
+import {shallow, mount} from 'enzyme'
+import {Provider} from 'react-redux'
 
 import {MemoryRouter as Router} from 'react-router-dom'
 
