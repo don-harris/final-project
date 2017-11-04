bce48749d4fd2da13e42c30f4615578f481c0ec3
Your Name <you@example.com>
2017-11-04 23:34:34 +1300

    last push before pull

--- client/components/Dictaphone.jsx
+++ client/components/Dictaphone.jsx
@@ -1,51 +1,27 @@
-import React, { PropTypes, Component } from 'react'
+import React, {PropTypes, Component} from 'react'
 import SpeechRecognition from 'react-speech-recognition'
 
-import {getVideos} from '../actions/videos'
 import {connect} from 'react-redux'
-import Video from './Video'
 const propTypes = {
-  // Props injected by SpeechRecognition
   transcript: PropTypes.string,
   startListening: PropTypes.func,
   stopListening: PropTypes.func,
   browserSupportsSpeechRecognition: PropTypes.bool
 }
 
-// var actual = 'Get to the chopper'
-// var actual = this.props.videos.quote
-
-// function straightLace (stopListening, transcript) {
-//   stopListening()
-//   console.log(transcript, actual)
-//   if (transcript.toLowerCase() === actual.toLowerCase()) {
-//     console.log('Correct!')
-//   } else {
-//     console.log("That's not it.")
-//   }
-// }
-
 class Dictaphone extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      randomVid: null
     }
   }
-  componentWillReceiveProps (nextProps) {
-    if (!this.state.randomVid) this.setState({randomVid: nextProps.videos[Math.floor(Math.random() * nextProps.videos.length)]})
-  }
-  componentDidMount () {
-    this.props.dispatch(getVideos())
-  }
   render () {
-    const { transcript, startListening, stopListening, browserSupportsSpeechRecognition } = this.props
-    const {randomVid} = this.state
+    const {transcript, startListening, stopListening, browserSupportsSpeechRecognition, randomVid} = this.props
     function compareText () {
       stopListening()
       var points = 0
-      var actual = randomVid.quote
-      console.log('quote from database = ', randomVid.quote)
+      var actual = randomVid
+      console.log('quote from database = ', this.props.quote)
       console.log('transcript = ' + transcript) // look at final transcript
       transcript.toLowerCase().split(' ').forEach((char, idx, transcriptArr) => {
         const actualArr = actual.toLowerCase().split(' ')
@@ -62,10 +38,8 @@ class Dictaphone extends Component {
     if (!browserSupportsSpeechRecognition) {
       return null
     }
-    // const {randomVid} = this.state
     return (
       <div>
-        {randomVid &&<Video randomVid={randomVid}/>}
         <button onClick={startListening}>Speak</button>
         <button onClick={compareText.bind(null, stopListening, transcript)}>Stop/Submit</button>
         <br/>
@@ -83,10 +57,4 @@ const options = {
   autoStart: false
 }
 
-const mapStateToProps = (state) => {
-  return {
-    videos: state.videos
-  }
-}
-
-export default connect(mapStateToProps)(SpeechRecognition(options)(Dictaphone))
+export default connect()(SpeechRecognition(options)(Dictaphone))
--- client/components/Round.jsx
+++ client/components/Round.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-import { startRound, nextPlayer } from '../actions/round'
+import {startRound, nextPlayer} from '../actions/round'
 import Dictaphone from './Dictaphone'
+import {getVideos} from '../actions/videos'
+import Video from './Video'
 
 class Round extends React.Component {
   constructor (props) {
@@ -10,7 +12,8 @@ class Round extends React.Component {
     this.state = {
       score: 10,
       video: 'funny cat',
-      id: 1
+      id: 1,
+      randomVid: null
     }
     this.handleClick = this.handleClick.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -20,8 +23,11 @@ class Round extends React.Component {
     const remainingPlayers = this.props.players.slice(1)
     this.props.dispatch(startRound(currentPlayer, remainingPlayers))
     window.localStorage.setItem('round', JSON.stringify(this.props.round))
+    this.props.dispatch(getVideos())
   }
-
+  componentWillReceiveProps (nextProps) {
+    if (!this.state.randomVid) this.setState({ randomVid: nextProps.videos[Math.floor(Math.random() * nextProps.videos.length)] })
+  }
   handleClick () {
     const {round} = this.props
     this.props.dispatch(nextPlayer(this.state, round.currentPlayer, round.remainingPlayers, round.roundNumber))
@@ -37,11 +43,14 @@ class Round extends React.Component {
   }
   render () {
     const {currentPlayer} = this.props.round
+    const {randomVid} = this.state
+    // console.log('quote from database = ', randomVid.quote)
     return (
       <div>
         <h1>Round Page</h1>
         {currentPlayer && <h2>{currentPlayer.name}</h2>}
-        <Dictaphone />
+        {randomVid && <Video randomVid={randomVid} />}
+        <Dictaphone randomVid={randomVid}/>
         <input onChange={this.handleChange} type="text" />
         <button id="next" className="button is large" onClick={this.handleClick}>
             Continue
@@ -54,7 +63,8 @@ class Round extends React.Component {
 const mapStateToProps = state => {
   return {
     players: state.players,
-    round: state.round
+    round: state.round,
+    videos: state.videos
   }
 }
 
