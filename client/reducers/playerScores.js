745b2aef55058ca1dfcef30ae96a0a87cb013ab7
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-11-04 18:26:22 +1300

    clear scores after game end

--- client/reducers/playerScores.js
+++ client/reducers/playerScores.js
@@ -1,9 +1,12 @@
 import {SCORE_EACH_ROUND} from '../actions/playerScores'
+import {RESET_GAME} from '../actions/round'
 
 export default function playerScores (state = [], action) {
   switch (action.type) {
     case SCORE_EACH_ROUND:
       return [...state, action.playerScores]
+    case RESET_GAME:
+      return []
     default:
       return state
   }
