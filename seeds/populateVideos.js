83657f88ec442347bdee851e3f543b942f015c7d
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2017-11-06 21:50:26 +1300

    changed the quote from 'gonna need' to 'going to need'

--- seeds/populateVideos.js
+++ seeds/populateVideos.js
@@ -16,7 +16,7 @@ exports.seed = function (knex, Promise) {
         {id: 12, vid_url: 'RKysEIVJfBs', quote: 'the names bond james bond', startTime: 67, quoteStart: 80, quoteEnd: 90, pauseTime: 10, title: 'Casino Royale'},
         {id: 13, vid_url: 'AyrP-pwDayE', quote: 'you had me at hello', startTime: 75, quoteStart: 91, quoteEnd: 94, pauseTime: 10, title: 'Jerry Maguire'},
         {id: 14, vid_url: 'vL-KQij0I8I', quote: 'from a pile of stuff', startTime: 84, quoteStart: 109, quoteEnd: 111, pauseTime: 10, title: 'The Devil Wears Prada'},
-        {id: 15, vid_url: 'oKxsW8DKJQQ', quote: 'you\'re gonna need a bigger boat', startTime: 57, quoteStart: 74, quoteEnd: 77, pauseTime: 10, title: 'Jaws'},
+        {id: 15, vid_url: 'oKxsW8DKJQQ', quote: 'you\'re going to need a bigger boat', startTime: 57, quoteStart: 74, quoteEnd: 77, pauseTime: 10, title: 'Jaws'},
         {id: 16, vid_url: '9FnO3igOkOk', quote: 'you can\'t handle the truth', startTime: 33, quoteStart: 43, quoteEnd: 47, pauseTime: 10, title: 'A Few Good Men'},
         {id: 17, vid_url: 'YbBiXPVKuTA', quote: 'are you not entertained', startTime: 70, quoteStart: 84, quoteEnd: 90, pauseTime: 10, title: 'Gladiator'},
         {id: 18, vid_url: 'luBk4VrMHNI', quote: 'become a pop star and they give you them for free', startTime: 12, quoteStart: 21, quoteEnd: 28, pauseTime: 10, title: 'Love Actually'}
