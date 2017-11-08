e0002dbb4542f7b29240a9d2dd4f2ed76df52912
richardjohns <you@example.com>
2017-11-08 19:41:33 +1300

    8 tests passed

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {shallow, mount} from 'enzyme'
+import { Provider } from "react-redux";
 import './setup-dom'
+import store from "../../client/store";
+import { MemoryRouter as Router } from "react-router-dom";
 
 import Homepage from '../../client/components/Homepage'
 import Instructions from "../../client/components/Instructions";
 import Header from "../../client/components/Header";
 import Leaderboard from "../../client/components/Leaderboard";
+import HallOfFame from "../../client/components/HallOfFame";
 
 test('Title renders on homepage', () => {
   const wrapper = shallow(<Homepage />)
@@ -19,5 +23,16 @@ test("Actors guide renders on instructions", () => {
 
 test("Title renders on Header component", () => {
   const wrapper = shallow(<Header />);
+  expect(wrapper.find("h1").first().text()).toBe("Spoken,")
+});
+
+test("Title renders on Hall of Fame", () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <Router>
+        <HallOfFame />
+      </Router>
+    </Provider>
+  );
   expect(wrapper.find("h1").first().text()).toBe("Spoken,")
 });
