dfac7ae8fab7aa2238d9d3b48ce41af50ad7f550
AnnahGerletti <agerletti@gmail.com>
2017-11-08 17:30:36 +1300

    mapping over profiles object

--- client/components/AboutUs.jsx
+++ client/components/AboutUs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from './Header'
 import AboutUsPeople from './AboutUsPeople'
+const data = require ('../data/profile')
 
 const AboutUs = props => {
   return <div>
@@ -9,8 +10,8 @@ const AboutUs = props => {
       <br />
       <Link to="/">
         <button className="button is-large is-danger">Back to Home</button>
-      </Link>
-      <br />
+     </Link>
+     <br />
       <div className="something">
         <p className="subtitle">
           We are five excited devs coming together to create the ultimate
@@ -21,11 +22,10 @@ const AboutUs = props => {
       </div>
 
       <div>
-        <AboutUsPeople name="Joe" email="joe@joe.com" githubUserName="JosephJvB" imageSrc="joe.jpg" />
-        <AboutUsPeople name="Annah" email="agerletti@gmail.com" githubUserName="AnnahGerletti" imageSrc="annah.jpg" />
-        <AboutUsPeople name="Don" email="donald.walter.harris@gmail.com" githubUserName="don-harris" imageSrc="donh.jpg" />
-        <AboutUsPeople name="Richard" email="richard@gmail.com" githubUserName="richardjohns" imageSrc="rich.jpg" />
-        <AboutUsPeople name="Callan" email="strettcall@yahoo.com" githubUserName="callan-stretton" imageSrc="cal.png" />
+        {data.profile.map((profile) => {
+          return <AboutUsPeople name={profile.name} email={profile.email} githubUserName={profile.githubUserName} imageSrc={profile.imageSrc} aboutMe={profile.aboutMe} />
+        })}
       </div>
-    </div>;}
+    </div>
+  }
 export default AboutUs
---
+++ client/data/profile.js
@@ -0,0 +1,41 @@
+
+module.exports ={
+  profile:[
+    { id: 1,
+      name: "Joe",
+      email: "jo-van-bo@hotmail.com",
+      githubUserName: "JosephJvB",
+      imageSrc: "joe.jpg",
+      aboutMe:"I'm just a guy who's a coder for fun. My top movies include: Reservoir Dogs, Supertroopers and Shrek 2. I had a blast on this project hope you enjoy it also."
+    },
+    { id: 2,
+      name: "Annah",
+      email: "agerletti@gmail.com",
+      githubUserName: "AnnahGerletti",
+      imageSrc: "annah.jpg",
+      aboutMe:"I am a fun loving and positive developer who took on this epic project with a smile. I was so excited to make a game and I think it has turned out amazing. I was part of a dedicated and adventurous team, using several different technologies we wanted to show an amazing and impressive final project for our EDA graduation."
+    },
+    { id: 3,
+      name: "Don",
+      email: "donald.walter.harris@gmail.com",
+      githubUserName: "don-harris",
+      imageSrc: "donh.jpg",
+      aboutMe:"I was project manager for this team. My main inspiration is Bill Murray. Coding is my new love and I like long walks on the beach. I may follow a career in A.I. but that's another conversation. I hope you enjoy the app."
+    },
+    { id: 4,
+      name: "Richrd",
+      email: "richard.johns193@gmail.com",
+      githubUserName: "richardjohns",
+      imageSrc: "rich.jpg",
+      aboutMe:"I brought a calmness to the team. I also was the man behind the podium."
+    },
+    { id: 5,
+      name: "Callan",
+      email: "strettcall@yahoo.com",
+      githubUserName: "callan-stretton",
+      imageSrc: "cal.png",
+      aboutMe:" I was the git master and not one piece of broken code got passed me."
+    }
+
+  ]
+}
