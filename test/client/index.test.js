6c76e818d974c8bde34681677c563e76dfa9f4e0
richardjohns <you@example.com>
2017-11-08 14:27:00 +1300

    add more initial header check tests

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {shallow, mount} from 'enzyme'
+import './setup-dom'
 
 import Homepage from '../../client/components/Homepage'
-import './setup-dom'
+import Instructions from "../../client/components/Instructions";
+import Header from "../../client/components/Header";
+import Leaderboard from "../../client/components/Leaderboard";
 
 Homepage.prototype.componentDidMount = () => {}
 
@@ -10,3 +13,29 @@ test('Title renders on homepage', () => {
   const wrapper = shallow(<Homepage />)
   expect(wrapper.find('h1').text()).toBe('Spoken, Not Stirred')
 })
+
+Instructions.prototype.componentDidMount = () => {};
+
+test("Actors guide renders on instructions", () => {
+  const wrapper = shallow(<Instructions />);
+  expect(wrapper.find("h1").first().text()).toBe("Actors Guide");
+});
+
+Header.prototype.componentDidMount = () => {};
+
+test("Title renders on Header component", () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find("h1").first().text()).toBe("Spoken,");
+});
+
+Leaderboard.prototype.componentDidMount = () => {};
+
+test("Title renders on Leaderboard component", () => {
+  const wrapper = shallow(<Leaderboard />);
+  expect(
+    wrapper
+      .find("h1")
+      .first()
+      .text()
+  ).toBe("Spoken,");
+});
